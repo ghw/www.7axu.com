0e936a105fd1e4020031cf14ce865cc4ef933458
Haowu Ge <ghw@7axu.com>
2015-08-22 12:56:21 +0800

    添加统计代码

---
+++ js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?8a635e738f5e7fc0ab99682de86ccbbe";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
